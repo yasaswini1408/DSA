d4581d2b10aeadb95be1c27c7985004c7a178163
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-02-04 23:24:40 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.5%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        long long i=1,l=0;
-        bool k=true;
-        while(k){
-            long long p=(i*(i+1))/2;
-            if(p<=n) l++,i++;
-            else k=false;
+        long long l = 1, r = n;
+        int ans = 1;
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            long long sum = (mid * (mid + 1)) / 2;
+            if (sum <= n) {
+                ans = mid;
+                l = mid + 1;
+            } else r = mid - 1;
         }
-        return l;
+        return ans;
+
+        // long long i=1,l=0;
+        // bool k=true;
+        // while(k){
+        //     long long p=(i*(i+1))/2;
+        //     if(p<=n) l++,i++;
+        //     else k=false;
+        // }
+        // return l;
     }
 };

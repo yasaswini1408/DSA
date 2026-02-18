96984143a85e00a3019ca4edab82bd5005aa1479
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-02-18 18:47:01 +0530

    Time: 0 ms (100%), Space: 7.7 MB (91.43%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        string s=bitset<64>(n).to_string();
-        s=s.substr(s.find_first_not_of('0'));
-        for(int i=0;i<s.size()-1;i++){
-            if(s[i]==s[i+1]) return false;
-        }
-        return true;
+        unsigned int x=n^(n>>1);
+        return (x&(x+1))==0;
+
+        // string s=bitset<64>(n).to_string();
+        // s=s.substr(s.find_first_not_of('0'));
+        // for(int i=0;i<s.size()-1;i++){
+        //     if(s[i]==s[i+1]) return false;
+        // }
+        // return true;
     }
 };

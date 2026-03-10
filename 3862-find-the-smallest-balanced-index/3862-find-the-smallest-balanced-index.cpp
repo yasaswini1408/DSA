abfaf9d3cdc5ad28d60f43bf25f745a313fa13cf
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-03-10 18:52:28 +0530

    Time: 0 ms (100%), Space: 192.4 MB (87.48%) - LeetHub

--- 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
@@ -2,21 +2,32 @@ class Solution {
 public:
     int smallestBalancedIndex(vector<int>& nums) {
 
-        vector<long long>suff(nums.size()+1,1);
+        long long sum=accumulate(nums.begin(),nums.end(),0LL);
+        long long pro=1;
         for(int i=nums.size()-1;i>=0;i--){
-            if(suff[i+1]>LLONG_MAX/nums[i]){
-                suff[i]=LLONG_MAX;
-            }else
-            suff[i]=suff[i+1]*nums[i];
-        }
-        long long lsum=0;
-        for(int i=0;i<nums.size();i++){
-            long long rpro=suff[i+1];
-            if(lsum==rpro) return i;
-
-            lsum+=nums[i];
+            if(sum-nums[i]==pro) return i;
+            if(pro>sum) break;
+            if(pro>(LLONG_MAX/nums[i])) break;
+            sum=sum-nums[i];
+            pro=pro*nums[i];
         }
         return -1;
+
+        // vector<long long>suff(nums.size()+1,1);
+        // for(int i=nums.size()-1;i>=0;i--){
+        //     if(suff[i+1]>LLONG_MAX/nums[i]){
+        //         suff[i]=LLONG_MAX;
+        //     }else
+        //     suff[i]=suff[i+1]*nums[i];
+        // }
+        // long long lsum=0;
+        // for(int i=0;i<nums.size();i++){
+        //     long long rpro=suff[i+1];
+        //     if(lsum==rpro) return i;
+
+        //     lsum+=nums[i];
+        // }
+        // return -1;
         
         // long long sum=0;
         // for(int i=0;i<nums.size();i++){

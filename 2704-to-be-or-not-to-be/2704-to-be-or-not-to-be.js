71bad121ae4e1a18becaa40828e666023a7fac9c
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-03-18 19:34:03 +0530

    Time: 56 ms (5.81%), Space: 52.6 MB (96.32%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    function toBe(n){
+        if(n===val) return true;
+        throw new Error("Not Equal")
+    }
+    function notToBe(n){
+        if(n!==val) return true;
+        throw new Error("Equal")
+    }
+    return {toBe,notToBe};
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */

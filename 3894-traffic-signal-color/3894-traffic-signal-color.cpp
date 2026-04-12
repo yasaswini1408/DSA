f7ac5cb769a89da308f3898046b50c1c72257cd3
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-04-12 11:45:55 +0530

    Time: 0 ms (100%), Space: 7.9 MB (57.14%) - LeetHub

---
+++ 3894-traffic-signal-color/3894-traffic-signal-color.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string trafficSignal(int timer) {
+        if(timer==0) return "Green";
+        else if(timer==30) return "Orange";
+        else if(timer>30 and timer<=90 ) return "Red";
+        else return "Invalid";
+    }
+};

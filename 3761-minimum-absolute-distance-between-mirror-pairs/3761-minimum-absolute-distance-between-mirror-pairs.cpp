3b3b4b98fbb1d22715436101e1eb9ded3276ac1a
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-04-17 16:17:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,54 +1,26 @@
 class Solution {
 public:
-    int reverse_digits(int n){
-        int val = 0;        
-        while(n){
-            val = val * 10 + n%10;
-            n = n/10;
-        }
-        return val;
-    }
     int minMirrorPairDistance(vector<int>& nums) {
+        int mini = INT_MAX;
         int n = nums.size();
-        unordered_map<int,int>mp;
-        int ans = n;
-        for(int i=0;i<n;i++){
-            if(mp.find(nums[i])!=mp.end()){
-                ans = min(ans,i-mp[nums[i]]);
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < n; i++) mp[nums[i]].push_back(i);
+        for (int i = 0; i < n; i++) {
+            string p = to_string(nums[i]);
+            reverse(p.begin(), p.end());
+            size_t k = p.find_first_not_of('0');
+            if (k == string::npos) p = "0";
+            else p = p.substr(k);
+            int val = stoi(p);
+            if (mp.count(val)) {
+                for (int j : mp[val]) {
+                    if (j > i) { 
+                        mini = min(mini, j - i);
+                        break; 
+                    }
+                }
             }
-            int val = reverse_digits(nums[i]);
-            mp[val] = i;
         }
-        return (ans==n ? -1 : ans);
+        return mini == INT_MAX ? -1 : mini;
     }
 };
-
-
-
-
-
-// class Solution {
-// public:
-//     int reverse(int n) {
-//         int rev = 0;
-//         while (n > 0) {
-//             rev = rev * 10 + (n % 10);
-//             n /= 10;
-//         }
-//         return rev;
-//     }
-
-//     int minMirrorPairDistance(vector<int>& n) {
-//         int r=INT_MAX,z;
-//         for(int i=0;i<n.size();i++){
-//             for(int j=i+1;j<n.size();j++){
-//                 int p=n[i];
-//                 int q=reverse(n[j]);
-//                 if(p==q||n[j]==reverse(n[i])) {
-//                     r=min(r,j-i);
-//                 }
-//             }
-//         }
-//         return (r == INT_MAX) ? -1 : r;
-//     }
-// };

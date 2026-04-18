c135ff78ca078e39f3b61d974894ba5c9d903f7c
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-04-18 12:28:43 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.72%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0,cand=-1;
+        for(int i:nums){
+            if(count==0) cand=i;
+            if(i==cand) count++;
+            else count--;
+        }
+        return cand;
+    }
+};

e0ca9ca9872ef368fbf1ef04da95aecb6233d0ec
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-05-14 23:23:32 +0530

    Time: 0 ms (100%), Space: 71.7 MB (51.12%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans=INT_MIN,curr=0;
+        for(int i=0;i<nums.size();i++){
+            curr=max(nums[i],curr+nums[i]);
+            ans=max(ans,curr);
+        }
+        return ans;
+    }
+};

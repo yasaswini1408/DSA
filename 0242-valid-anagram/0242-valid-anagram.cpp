dc75ba6787371c8ef818d0d858c77eb03ebb9753
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-05-15 12:21:08 +0530

    Time: 5 ms (25.46%), Space: 9.7 MB (48.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -8,16 +8,16 @@ public:
        
         return ss==tt;
 
-        // if (s.length() != t.length()) return false;
-        // int arr[26] = {0};
-        // for (int i = 0 ; i<s.length(); i++){
-        //     arr[s[i]-'a'] += 1;
-        //     arr[t[i] - 'a'] -= 1;
-        // }
-        // for (int check : arr){
-        //     if (check != 0) return false;
-        // }
-        // return true;
+        if (s.length() != t.length()) return false;
+        int arr[26] = {0};
+        for (int i = 0 ; i<s.length(); i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+        for (int check : arr){
+            if (check != 0) return false;
+        }
+        return true;
         
         // sort(s.begin(),s.end());
         // sort(t.begin(),t.end());

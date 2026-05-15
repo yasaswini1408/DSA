0594220abe7b5ef853b1e147aa1da6573036bfd3
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-05-15 12:19:08 +0530

    Time: 8 ms (6.94%), Space: 9.8 MB (48.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
-        int arr[26] = {0};
-        for (int i = 0 ; i<s.length(); i++){
-            arr[s[i]-'a'] += 1;
-            arr[t[i] - 'a'] -= 1;
-        }
-        for (int check : arr){
-            if (check != 0) return false;
-        }
-        return true;
+        map<char,int>ss,tt;
+        for(auto i:s) ss[i]++;
+        for(auto i:t) tt[i]++;
+        if(ss.size()!=tt.size()) return false;
+       
+        return ss==tt;
+
+        // if (s.length() != t.length()) return false;
+        // int arr[26] = {0};
+        // for (int i = 0 ; i<s.length(); i++){
+        //     arr[s[i]-'a'] += 1;
+        //     arr[t[i] - 'a'] -= 1;
+        // }
+        // for (int check : arr){
+        //     if (check != 0) return false;
+        // }
+        // return true;
         
         // sort(s.begin(),s.end());
         // sort(t.begin(),t.end());

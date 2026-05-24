9d8bb11bc8fc1b55ae65f4e67a25c80192c4a0bc
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-05-24 12:35:37 +0530

    Time: 0 ms (100%), Space: 33.9 MB (9.12%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.cpp
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<int> limitOccurrences(vector<int>& nums, int k) {
         unordered_map<int,int>mp;
-        set<int>s(nums.begin(),nums.end());
-        for(auto i:nums) mp[i]++;
-        vector<int>res;
-        for(auto i:s){
-            if(mp[i]>=k) {
-                for(int j=0;j<k;j++) res.push_back(i);
-            }else{
-                for(int j=0;j<mp[i];j++) res.push_back(i);
+        vector<int>ans;
+        for(int x:nums){
+            if(mp[x]<k){
+                ans.push_back(x);
+                mp[x]++;
             }
         }
-        return res;
+        return ans;
     }
 };

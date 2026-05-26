ff449f3198099ed3c85c79f14487c4a3cf13d04b
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-05-26 12:30:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (86.08%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<int>lower(26,0),upper(26,0);
-        for(int i=0;i<word.size();i++){
-            if(islower(word[i])) lower[word[i]-'a']++;
-            else upper[word[i]-'A']++;
+        int lower = 0, upper = 0;
+        for(char ch : word) {
+            if(islower(ch))
+                lower |= (1 << (ch - 'a'));
+            else
+                upper |= (1 << (ch - 'A'));
         }
-        int c=0;
-        for(int i=0;i<lower.size();i++){
-            if(lower[i]>=1 and upper[i]>=1) c++;
-        }
-        return c;
+        return __builtin_popcount(lower & upper);
     }
 };

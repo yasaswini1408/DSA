843b7d06dfafdbe95799480b9ccc318823baa872
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-05-30 15:05:45 +0530

    Time: 590 ms (10.32%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,5 +2,6 @@
 select w2.id
 from weather w1 
 join weather w2
-on w2.recordDate = DATE_ADD(w1.recordDate, INTERVAL 1 DAY) 
+-- on w2.recordDate = DATE_ADD(w1.recordDate, INTERVAL 1 DAY) 
+on DATEDIFF(w2.recordDate, w1.recordDate) = 1
 where w1.temperature<w2.temperature;

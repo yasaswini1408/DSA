3ef641913d3a9f26e7f1a99536a32a63193fdc53
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-07-12 20:19:20 +0530

    Time: 40 ms (42.82%), Space: 45.3 MB (20.8%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int>res=arr,ans;
-        sort(res.begin(),res.end());
-        map<int,int>m;
-        int c=1;
-        for(int i=0;i<res.size();i++){
-            if(m.find(res[i])==m.end())
-                m[res[i]]=c++;
+        // vector<int>res=arr,ans;
+        // sort(res.begin(),res.end());
+        // map<int,int>m;
+        // int c=1;
+        // for(int i=0;i<res.size();i++){
+        //     if(m.find(res[i])==m.end())
+        //         m[res[i]]=c++;
+        // }
+        // for(auto i:arr){
+        //     ans.push_back(m[i]);
+        // }
+        // return ans;
+
+
+        vector<int> res = arr;
+        sort(res.begin(), res.end());
+        res.erase(unique(res.begin(), res.end()), res.end());
+        unordered_map<int,int> rank;
+        for(int i=0; i<res.size(); i++){
+            rank[res[i]] = i+1; 
         }
-        for(auto i:arr){
-            ans.push_back(m[i]);
+        vector<int> ans;
+        for(auto x : arr){
+            ans.push_back(rank[x]);
         }
         return ans;
     }

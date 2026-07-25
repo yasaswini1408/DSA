4439f6009f6d58d2cd92c6d3a309cf29eaf281ba
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-07-25 23:39:36 +0530

    Time: 7 ms (34.35%), Space: 82.3 MB (50.56%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
-        int n = nums.size();
-        vector<long long> p(n);
-        p[0] = nums[0];
-        for (int i = 1; i < n; i++)
-            p[i] = p[i - 1] + nums[i];
-        vector<long long> s(n);
-        s[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--)
-            s[i] = s[i + 1] + nums[i];
+        long long total = 0;
+        for (int x : nums)
+            total += x;
+        long long left = 0;
+        long long best = LLONG_MAX;
         int ans = 0;
-        long long mn = LLONG_MAX;
+        int n = nums.size();
         for (int i = 0; i < n; i++) {
-            long long leftAvg = p[i] / (i + 1);
+            left += nums[i];
+            long long leftAvg = left / (i + 1);
             long long rightAvg = 0;
             if (i != n - 1)
-                rightAvg = s[i + 1] / (n - i - 1);
+                rightAvg = (total - left) / (n - i - 1);
             long long diff = llabs(leftAvg - rightAvg);
-            if (diff < mn) {
-                mn = diff;
+            if (diff < best) {
+                best = diff;
                 ans = i;
             }
         }
-
         return ans;
     }
 };

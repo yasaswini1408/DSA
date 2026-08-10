6bee1edade022fe902651aa518687b3688f64c80
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-08-10 19:50:06 +0530

    Time: 0 ms (100%), Space: 9.4 MB (84.96%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int zero=0,one=0;
+        int c=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1') one++;
+            else zero++;
+            if(abs(one-zero)==1 or abs(one-zero)==0) c++;
+        }
+        return c;
+    }
+};

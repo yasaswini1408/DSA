675ac14e44e62c01e4405027d865b03fbd3813a5
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-08-28 22:46:53 +0530

    Time: 19 ms (80.98%), Space: 17.8 MB (84.12%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(auto c:coins){
+                if(c>i) continue;
+                else{
+                    dp[i]=min(dp[i],1+dp[i-c]);
+                }
+            }
+        }
+        return dp[amount]==amount+1? -1:dp[amount];
+    }
+};

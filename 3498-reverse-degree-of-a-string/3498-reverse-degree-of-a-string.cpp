7676599da084359b72e584fae84eb7fe71318a7f
Samala Yasaswini <yasaswinisamala2005@gmail.com>
2026-09-20 23:40:42 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int reverseDegree(string s) {
         int ss=0;
         for(int i=1;i<=s.size();i++){
-            cout<<26-s[i-1]+'a'<<" ";
+            // cout<<26-s[i-1]+'a'<<" ";
             ss+=i*(26-s[i-1]+'a');
         }
         return ss;
